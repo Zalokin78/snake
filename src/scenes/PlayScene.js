a7bc6340f9468590effe3890bcc1ac9d119fcb32
zalokin <nicholasgalliano@yahoo.co.uk>
2024-03-24 23:22:13 +0100

    working with delays
    playing around with the time values to achieve the segment to follow the head in a delayed pos

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -8,6 +8,9 @@ class Snake extends Phaser.GameObjects.GameObject {
     this.xPos = 100;
     this.yPos = 400;
     this.segments = this.scene.physics.add.group();
+    this.timeSwitch = true;
+    this.delayTime = null;
+    this.delayedSegment = null;
   }
   /*  preload() {
     this.image = this.load.image("segment", "assets/snake16Arrow.png");
@@ -23,6 +26,10 @@ class Snake extends Phaser.GameObjects.GameObject {
     this.head = this.scene.physics.add
       .sprite(200, 200, "segment")
       .setVelocity(100, 0);
+
+    this.segment = this.scene.physics.add
+      .sprite(200, 200, "segment")
+      .setVelocity(0, 0);
 
     //this.cursors = this.scene.input.keyboard.createCursorKeys();
 
@@ -40,12 +47,32 @@ class Snake extends Phaser.GameObjects.GameObject {
   }
 
   update() {
+    if (this.timeSwitch) {
+      this.delayTime =
+        this.scene.game.loop.time * this.scene.game.loop.delta + 2000;
+
+      this.timeSwitch = false;
+      //debugger;
+    }
+    this.delayedSegment = { x: this.head.x, y: this.head.y };
+    if (
+      this.scene.game.loop.time * this.scene.game.loop.delta >=
+      this.delayTime
+    ) {
+      console.log("YOU'RE DELAYED!!!!!");
+      this.segment.x = this.delayedSegment.x;
+      this.segment.y = this.delayedSegment.y;
+      this.timeSwitch = true;
+    }
+    //console.log("hello");
+
     this.scene.physics.velocityFromAngle(
       this.head.angle,
       150,
       this.head.body.velocity
     );
 
+    //console.log(this.scene.time);
     // console.log(this.head.distance.length);
     //this.head.setVelocityX(10);
     //this.head.rotation = this.head.angle;
@@ -97,8 +124,15 @@ class PlayScene extends Phaser.Scene {
     this.snakeA.create();
   }
 
-  update(time, delta) {
+  update(/* time, delta */) {
+    // console.log("time " + time);
+    // console.log("delta " + delta);
+
     this.snakeA.update();
+    //console.log(this.game.loop.actualFps);
+    //console.log(this.game.loop.time);
   }
 }
+
+/*var game = new Phaser.Game(800, 600, Phaser.AUTO, { preload: preload, create: create, update: update });function preload() {game.load.bitmapFont('desyrel', '/assets/fonts/desyrel.png', '/assets/fonts/desyrel.xml');}var textStyle = { font: '64px Desyrel', align: 'center'};var timer;var milliseconds = 0;var seconds = 0;var minutes = 0;function create() {timer = game.add.bitmapText(250, 250, '00:00:00', textStyle);}function update() {//Calling a different function to update the timer just cleans up the update loop if you have other code.updateTimer();}function updateTimer() {minutes = Math.floor(game.time.time / 60000) % 60;seconds = Math.floor(game.time.time / 1000) % 60;milliseconds = Math.floor(game.time.time) % 100;//If any of the digits becomes a single digit number, pad it with a zeroif (milliseconds < 10)milliseconds = '0' + milliseconds;if (seconds < 10)seconds = '0' + seconds;if (minutes < 10)minutes = '0' + minutes;timer.setText(minutes + ':'+ seconds + ':' + milliseconds);}*/
 export default PlayScene;
